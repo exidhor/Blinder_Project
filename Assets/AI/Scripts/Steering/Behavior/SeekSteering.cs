90e0a7d60b3dfaa64cac481e0a4b3fda3db66caa
Jipay <jipay.detiege@gmail.com>
2017-06-24 14:53:40 +0200

    Add foldr in steering

--- Assets/AI/Scripts/Steering/Behavior/SeekSteering.cs
+++ Assets/AI/Scripts/Steering/Behavior/SeekSteering.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using BlinderProject;
+﻿using System.Collections.Generic;
 using MapEditor;
 using Pathfinding;
 using Tools;
